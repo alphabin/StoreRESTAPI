150c88ec0e6560548934444efd7ecc02b80f521e
EC2 Default User <ec2-user@ip-172-31-40-106.us-east-2.compute.internal>
2019-07-31 21:21:52 +0000

    adding item_ctroller

---
+++ pos-API/app/controllers/item_controller.rb
@@ -0,0 +1,57 @@
+ class Item_Subsystem
+  def new_Item	
+    @item = Item.new
+    update(@item) 
+    if @item.save
+      render "item.json.jbuilder", status: 201
+    else
+      render(json: {messages: @item.errors.messages}, status: 400)
+    end
+  end
+  
+  def retrieve_Item
+    @item = Item.find_by(id: params[:id])
+    if @item.nil?
+       head 404  
+    else 
+      render "item.json.jbuilder"
+    end 
+  end
+
+  def update_Item 
+    @item = Item.find_by(id: params[:id])
+    if @item.nil?
+      render 404 
+    else 
+      update(@item)
+      if @item.save()
+         head 204
+      else 
+        render(json: {messages: @item.errors.messages}, status: 400)    
+      end 
+    end 
+  end
+
+  def order_Item
+    @item = Item.find_by(:id => params[:itemId])
+    if @item.nil?
+       head 404
+       return
+    end
+    if @item.stockQty > 0
+      @item.stockQty = @item.stockQty - 1
+      @item.save
+      head 204
+    else
+      render(json: {messages: [ {:id => ['Item not in stock'] }  ] }, status: 400 )
+    end 
+  end 
+	
+  def update(item)
+    item.id = params[:id] if params[:id].present?
+    item.description = params[:description] if params[:description].present?
+    item.price = params[:price].to_f if params[:price].present?
+    item.stockQty = params[:stockQty].to_i if params[:stockQty].present?
+  end 
+
+end
